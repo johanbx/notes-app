9b44dd2afd4c922c9f7db17f7686aaa18ac0dcab
Johan Borgenstierna <johan.bx@yubico.com>
2022-09-04 11:36:59 +0200

    linted

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,11 +2,11 @@
 // for information about these interfaces
 // and what to do when importing types
 declare namespace App {
-	interface Locals {
-		userid: string;
-	}
+  interface Locals {
+    userid: string;
+  }
 
-	// interface PageData {}
+  // interface PageData {}
 
-	// interface Platform {}
+  // interface Platform {}
 }
