70e214e625ae57b376d017f50a0dcc1285248b71
Johan Borgenstierna <johan.bx@yubico.com>
2022-09-04 12:00:00 +0200

    use cloudflare adapter

--- src/app.d.ts
+++ src/app.d.ts
@@ -8,5 +8,13 @@ declare namespace App {
 
   // interface PageData {}
 
-  // interface Platform {}
+  interface Platform {
+    env: {
+      COUNTER: DurableObjectNamespace
+    }
+    context: {
+      waitUntil(promise: Promise<any>): void;
+    }
+    caches: CacheStorage & { default: Cache }
+  }
 }
